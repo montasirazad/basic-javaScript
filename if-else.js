b20db6518afab3ce406322a09db199a263eab31f
unknown <montasirazad@gmail.com>
2021-06-23 02:54:06 +0600

    2:54am

---
+++ if-else.js
@@ -0,0 +1,26 @@
+function price (tk) {
+  if (tk<=80000) {
+      console.log("I will buy the camera.")
+      
+  }
+
+  else{
+      console.log("I will buy the laptop.")
+  }
+
+
+}
+price(150000)
+
+
+let steak=(price)=> {
+  if (price < 1000) {
+      console.log("I will eat steak.")
+  }
+
+  else{
+      console.log("I wont eat steak.")
+  }
+}
+
+steak(500);
